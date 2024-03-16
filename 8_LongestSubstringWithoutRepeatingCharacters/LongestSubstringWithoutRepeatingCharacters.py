943ed9a72c17543adddf6f00b0f1d42575ce32c4
Athena Rubio <75556016+SpeedyDolphin@users.noreply.github.com>
2024-03-15 19:43:45 -0700

    An attempt, this apporach may be flawed

---
+++ 8_LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,62 @@
+
+
+def lengthOfLongestSubstring(s, debug):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if(len(s) == 0):
+            return 0
+
+        memoryBank = dict()
+        lastResetChar = None
+        start = 0
+        longest = 1
+
+        for i in range(len(s)):
+            if debug: 
+                print(f'Looking at {s[i]}:{i}')
+            #Reset 
+            if s[i] in memoryBank.keys() and (memoryBank[s[i]][0] >= start or s[i] == lastResetChar):
+                if debug:
+                    print(f"Resetting - Conflict {s[i]}:{memoryBank[s[i]]} - Start: {start}")
+                # Potential pattern a****a******a
+                if lastResetChar == s[i] and s[i] != s[i-1]:
+                    #pattern a****a******a
+                    length = i - memoryBank[s[i]][0] -1
+                    memoryBank[s[i]] = [memoryBank[s[i]][1], i]
+                
+                # Pattern a***a
+                else:
+                    # print('~~general reset~~')      
+                    length = i-start
+                    lastResetChar = s[i]
+                    memoryBank[s[i]].append(i)
+
+                #General
+                if debug:
+                    print(f'New : {length}, Old: {longest}')     
+                longest = length if (length > longest) else longest
+                start = i
+            #Update key
+            else:
+                memoryBank[s[i]] = [i]
+            
+        if debug:
+            print(memoryBank)
+            print(f'Last entry : {len(s[start:])} - {s[start:]}')
+            print(f'Current Longest: {longest}')
+        return len(s)-start if (len(s)-start > longest) else longest
+
+
+if __name__ == "__main__":
+    s1 = 'abcabcbb' # expected 3
+    au = 'au'
+    s = 'dvdf'
+    # s = 'asjrgapa'
+    # s= 'pwwkew'
+    # s = 'bbbbbbbbbbbbbbbbb'
+    # s = "loddktdji"
+    print(lengthOfLongestSubstring(s1, False))
+    print(lengthOfLongestSubstring(s1[::-1], False))
+
